7546a8b91f6cae28bcc699e4fbc81931276db3e3
Jaimy Monsuur <jaimymonsuur@gmail.com>
2021-09-15 15:31:50 +0200

    update

--- Garden Group Project/DAL/Base.cs
+++ Garden Group Project/DAL/Base.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
 
 namespace DAL
 {
-    public class Class1
+    public abstract class Base
     {
+        public string connectionString;
+        public MongoClient dbClient;
+        public IMongoDatabase db;
+        public Base()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Project"].ConnectionString;
+            dbClient = new MongoClient(connectionString);
+            db = dbClient.GetDatabase("Garden_Goup");
+        }
     }
 }
---
+++ Garden Group Project/Model/Incident_Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Incident_Ticket
+    {
+        public int objectId;
+        public int id;
+        public DateTime reportDate;
+        public string subjectOfIncident;
+        public Incident_Type Incident_Type;
+        public string ReportedBy;
+        public Incident_Priority Incident_Priority;
+        public DateTime Deadline;
+        public string Description;
+    }
+}
