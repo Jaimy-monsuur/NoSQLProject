3bdb34cb34820be2b30cb748363cbcbebc23c2d6
Jaimy Monsuur <jaimymonsuur@gmail.com>
2021-09-24 13:17:43 +0200

    it work

---
+++ Garden Group Project/GardenGroupUI/Incident_Management.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GardenGroupUI
+{
+    public partial class Incident_Management : Form
+    {
+        public Incident_Management()
+        {
+            InitializeComponent();
+        }
+
+        private void PNLmain_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, PNLmain.ClientRectangle, Color.Black, ButtonBorderStyle.Solid);
+        }
+
+        private void menuStrip1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, Menu.ClientRectangle, Color.Black, ButtonBorderStyle.Solid);
+        }
+
+        private void Incident_Management_Load(object sender, EventArgs e)
+        {
+            foreach (ToolStripItem item in Menu.Items)
+            {
+                item.Size = new Size(415,24);
+            }
+        }
+    }
+}
